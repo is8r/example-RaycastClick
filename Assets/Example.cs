6ec9c3142d29857613f06d5d0ab1cad13f03cf29
Yu Ishihara <ishihara.yu@gmail.com>
2020-02-15 21:22:00 +0900

    Add files

---
+++ Assets/Example.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    void Update()
+    {
+        //マウスをクリックした場合
+        if (Input.GetMouseButtonDown(0))
+        {
+            //カメラの位置からマウスの位置に向けてレイを飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            //レイがヒットした場合
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                //ヒットしたオブジェクトのタグ名がCubeの場合
+                if (hit.collider.gameObject.CompareTag("Cube"))
+                {
+                    print("クリックされました");
+                }
+            }
+        }
+    }
+}
